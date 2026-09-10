a34fcddae4f12f9322e7734281aaefeb8f44b77f
EmreKaplan67 <emrekaplan96@gmail.com>
2026-09-10 14:51:14 +0100

    cors setup done

--- src/blog/app.py
+++ src/blog/app.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://blog-frontend-react-black.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
