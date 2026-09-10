df26488a270644b087529ce447851dbc91419b8d
EmreKaplan67 <emrekaplan96@gmail.com>
2026-09-10 14:30:37 +0100

    init.py changed for deployement

--- src/blog/__init__.py
+++ src/blog/__init__.py
@@ -1,7 +1,8 @@
 import uvicorn
+import os
 
 def main() -> None:
-    uvicorn.run("blog.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("blog.app:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
     
 if __name__ == "__main__":
     main()
